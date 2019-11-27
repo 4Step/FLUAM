cf6a4333e2f0ef75120a72dd2daf2237663a3b55
venkat <venkat.sarvepalli@dot.state.fl.us>
2019-11-27 13:25:21 -0500

    Bug fix in reporting non-res developed land and total (only effects summary tables)

--- source_code_new/9_Summary_Land_Consumption.R
+++ source_code_new/9_Summary_Land_Consumption.R
@@ -5,19 +5,29 @@
 # "9_Summary_Land_Consumption.R"
 
 summariseLandByCategory <- function(df){
+
+   # Recompute total acres (just to be sure)  
+   df <- df %>%   
+      mutate(TotalArea = resAvailableAcres +
+                nonresAvailableAcres +
+                resDeveloped +
+                nonresDeveloped +
+                undevelopableAcres 	+
+                AgriculturalAcres)
+
   # Coutywide land category summary
   lc_county <- df %>% 
-      group_by(growthCenter) %>%
+      group_by(growthCenter)  %>%
       summarise_at( vars("housing", "employment", "resAvailableAcres","nonresAvailableAcres",	 
                          "resDeveloped" , "nonresDeveloped" ,
-                         "undevelopableAcres", "AgriculturalAcres") , sum) %>%
+                         "undevelopableAcres", "AgriculturalAcres", "TotalArea") , sum) %>%
       ungroup()
   
    # Statewide land category summary
    lc_state <- df %>% 
       summarise_at( vars("housing", "employment", "resAvailableAcres","nonresAvailableAcres",	 
                          "resDeveloped" , "nonresDeveloped" ,
-                         "undevelopableAcres", "AgriculturalAcres") , sum)
+                         "undevelopableAcres", "AgriculturalAcres", "TotalArea") , sum)
    
    ret <- list(lc_state = lc_state, lc_county = lc_county)
    return(ret)
